aec84c0a3b59842b08de649b2b8b46814ecd441f
lvbu <634624413@qq.com>
2013-06-27 16:44:56 +0800

    11
    11

--- BaseViewController.m
+++ BaseViewController.m
@@ -28,6 +28,7 @@
 {
     NSLog(@"zhangweisenond");
     NSLog(@"zhegezhenmeiyou");
+    NSLog(@"123");
     imgViewOne=[UIButton buttonWithType:UIButtonTypeRoundedRect];
     [imgViewOne setFrame:CGRectMake(70, 100, 200, 200)];
     
