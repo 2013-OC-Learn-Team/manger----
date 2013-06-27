4cdbe266928210938d8b586951d8b5717c0a1132
sunaow <975807058@qq.com>
2013-06-27 17:12:42 +0800

    dddd
    dddd

--- BaseViewController.m
+++ BaseViewController.m
@@ -26,6 +26,7 @@
 
 -(void)viewWillAppear:(BOOL)animated
 {
+    NSLog(@"123");
     imgViewOne=[UIButton buttonWithType:UIButtonTypeRoundedRect];
     [imgViewOne setFrame:CGRectMake(70, 100, 200, 200)];
     
